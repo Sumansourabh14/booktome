398af9d23e05adda46b2665ec2dc2c69f37dab09
Suman Sourabh <suman.sourabh@ansrsource.com>
2024-02-03 13:30:51 +0530

    added sign up page

--- components/buttons/BigButton.js
+++ components/buttons/BigButton.js
@@ -1,9 +1,7 @@
-import React from "react";
-
-const BigButton = () => {
+const BigButton = ({ text }) => {
   return (
     <button className="px-12 py-2 bg-black text-white font-dmSerifText tracking-wider text-xl hover:bg-slate-900">
-      Sign Up
+      {text}
     </button>
   );
 };
--- components/pageComponents/HeroSection.js
+++ components/pageComponents/HeroSection.js
@@ -1,11 +1,16 @@
-import BigButton from "../buttons/BigButton";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
     <div className="flex flex-col items-center gap-8">
       <h1 className="font-dmSerifText text-8xl">booktome.</h1>
       <h2 className="text-xl">Get started</h2>
-      <BigButton />
+      <Link
+        href={`/signup`}
+        className="px-12 py-2 bg-black text-white font-dmSerifText tracking-wider text-xl hover:bg-slate-900"
+      >
+        Sign up
+      </Link>
     </div>
   );
 };
